99943fb8813727d7d91cb56a9c07c12cdbfe5820
Baochuan Lu <lubaochuan@gmail.com>
2020-11-11 11:22:42 -0600

    get category titles from db

---
+++ db_script.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `categories` (
+ `cat_id` int(11) NOT NULL AUTO_INCREMENT,
+ `cat_title` varchar(255) NOT NULL,
+ PRIMARY KEY (`cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4
--- includes/navigation.php
+++ includes/navigation.php
@@ -13,6 +13,14 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+        <?php
+          $query = "SELECT * FROM categories";
+          $select_all_categories_query = mysqli_query($connection, $query);
+          while($row = mysqli_fetch_assoc($select_all_categories_query)){
+            $cat_title = $row['cat_title'];
+            echo "<li>{$cat_title}</li>";
+          }
+        ?>
         <li>
           <a href="#">About</a>
         </li>
