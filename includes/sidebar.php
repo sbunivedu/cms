a29b2dd9a4a64d1b2032fa855f039bc55bc2fa13
Baochuan Lu <lubaochuan@gmail.com>
2020-12-03 10:36:37 -0600

    add search result to sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,24 +1,9 @@
 <div class="col-md-4">
-  <?php
-    if(isset($_POST['submit'])){
-      $search = $_POST['search'];
-      $query = "SELECT * FROM post WHERE post_tags LIKE '%$search%'";
-      $search_query = mysqli_query($connection, $query);
-      if(!$search_query){
-        die("Query failed".mysql_error($connection));
-      }
-      $count = mysqli_num_rows($search_query);
-      if($count == 0){
-        echo "<h1>No result</h1>";
-      }else{
-        echo "<h1>Some result</h1>";
-      }
-    }
-  ?>
+
   <!-- Blog Search Well -->
   <div class="well">
     <h4>Blog Search</h4>
-    <form action="" method="post">
+    <form action="search.php" method="post">
     <div class="input-group">
       <input name="search" type="text" class="form-control">
       <span class="input-group-btn">
