cfcfa0aa645c1c9d4f685c6ed40b888b133621db
Baochuan Lu <lubaochuan@gmail.com>
2020-12-08 14:34:09 -0600

    add categories

--- admin/categories.php
+++ admin/categories.php
@@ -16,7 +16,22 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">
-                        <form action="">
+<?php
+if(isset($_POST['submit'])){
+  $cat_title = $_POST['cat_title'];
+  if($cat_title == "" || empty($cat_title)){
+    echo "This field should not be empty";
+  }else{
+    $query = "INSERT INTO categories(cat_title) ";
+    $query .= "VALUE('{$cat_title}')";
+    $create_category_query = mysqli_query($connection, $query);
+    if(!$create_category_query){
+      die('QUERY FAILED'.mysqli_error($connection));
+    }
+  }
+}
+ ?>
+                        <form action="" method="post">
                           <div class="form-group">
                             <label for="cat-title">Add Category</label>
                             <input class="form-control" type="text" name="cat_title">
@@ -28,7 +43,7 @@
                       </div>
                       <div class="col-xs-6">
 <?php
-$query = "SELECT * FROM categories LIMIT 3";
+$query = "SELECT * FROM categories";
 $select_categories = mysqli_query($connection, $query);
  ?>
                         <table class="table table-border table-hover">
