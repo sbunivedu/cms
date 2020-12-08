7659c6504ddd7b8cca1b2e05ddb9b329e9617934
Baochuan Lu <lubaochuan@gmail.com>
2020-12-08 13:36:33 -0600

    clean up left menu bar for admin

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -35,22 +35,21 @@
                 <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
             <li>
-                <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                <ul id="posts_dropdown" class="collapse">
+                    <li>
+                        <a href="#">View All Posts</a>
+                    </li>
+                    <li>
+                        <a href="#">Add Posts</a>
+                    </li>
+                </ul>
             </li>
             <li>
-                <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
+                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories </a>
             </li>
             <li>
-                <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-            </li>
-            <li>
-                <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-            </li>
-            <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-            </li>
-            <li>
-                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="demo" class="collapse">
                     <li>
                         <a href="#">Dropdown Item</a>
@@ -61,10 +60,10 @@
                 </ul>
             </li>
             <li class="active">
-                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments </a>
             </li>
             <li>
-                <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+                <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Profile </a>
             </li>
         </ul>
     </div>
