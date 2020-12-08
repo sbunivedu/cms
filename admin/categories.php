687c748bf15f15643f0b47233e4a07d2c0c5f67e
Baochuan Lu <lubaochuan@gmail.com>
2020-12-08 15:51:23 -0600

    update categories

--- admin/categories.php
+++ admin/categories.php
@@ -40,6 +40,13 @@ if(isset($_POST['submit'])){
                             <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                           </div>
                         </form><!-- Add Category Form -->
+
+<?php
+if(isset($_GET['edit'])){
+    $cat_id = $_GET['edit'];
+    include "includes/update_categories.php";
+}
+ ?>
                       </div>
                       <div class="col-xs-6">
                         <table class="table table-border table-hover">
@@ -62,10 +69,15 @@ while($row = mysqli_fetch_assoc($select_categories)){
                              <td><?=$cat_id?></td>
                              <td><?=$cat_title?></td>
                              <td>
-                               <a href="categories.php?delete=<?=$cat_id?>">delete</a></td>
+                               <a href="categories.php?delete=<?=$cat_id?>">delete</a>
+                             </td>
+                             <td>
+                               <a href="categories.php?edit=<?=$cat_id?>">edit</a>
+                             </td>
                            </tr>
 <?php
 }
+// DELETE QUERY
 if(isset($_GET['delete'])){
   $the_cat_id = $_GET['delete'];
   $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
