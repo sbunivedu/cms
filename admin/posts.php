6d23596dc2b09df3b25f7d235aa7896fd6714b0c
Baochuan Lu <lubaochuan@gmail.com>
2020-12-10 10:28:43 -0600

    read posts from db

--- admin/posts.php
+++ admin/posts.php
@@ -29,17 +29,35 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>10</td>
-                <td>author</td>
-                <td>title</td>
-                <td>bootstrap</td>
-                <td>status</td>
-                <td>Image</td>
-                <td>tags</td>
-                <td>comments</td>
-                <td>date</td>
-              </tr>
+<?php
+$query = "SELECT * FROM posts";
+$select_posts = mysqli_query($connection, $query);
+while($row = mysqli_fetch_assoc($select_posts)){
+  $post_id = $row['post_id'];
+  $post_author = $row['post_author'];
+  $post_title = $row['post_title'];
+  $post_category_id = $row['post_category_id'];
+  $post_status = $row['post_status'];
+  $post_image = $row['post_image'];
+  $post_tags = $row['post_tags'];
+  $post_comment_count = $row['post_comment_count'];
+  $post_date = $row['post_date'];
+?>
+<tr>
+  <td><?= $post_id ?></td>
+  <td><?= $post_author ?></td>
+  <td><?= $post_title ?></td>
+  <td><?= $post_category_id ?></td>
+  <td><?= $post_status ?></td>
+  <td><img class = 'img-responsive' src = '../images/<?= $post_image ?>'</img></td>
+  <td><?= $post_tags ?></td>
+  <td><?= $post_comment_count ?></td>
+  <td><?= $post_date ?></td>
+</tr>
+<?php
+}
+?>
+
             </tbody>
           </table>
         </div>
