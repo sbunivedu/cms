f5bc015dd4ca363a06d192a6e11dbf1c0737a83e
Baochuan Lu <lubaochuan@gmail.com>
2020-12-10 10:12:17 -0600

    add table to display posts for admin

---
+++ admin/posts.php
@@ -0,0 +1,52 @@
+<?php include "includes/admin_header.php" ?>
+<?php include "functions.php" ?>
+
+<div id="wrapper">
+
+<?php include "includes/admin_navigation.php" ?>
+
+  <div id="page-wrapper">
+    <div class="container-fluid">
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+            Welcome to admin
+            <small>Author</small>
+          </h1>
+          <table class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>10</td>
+                <td>author</td>
+                <td>title</td>
+                <td>bootstrap</td>
+                <td>status</td>
+                <td>Image</td>
+                <td>tags</td>
+                <td>comments</td>
+                <td>date</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+            <!-- /.container-fluid -->
+    </div>
+        <!-- /#page-wrapper -->
+</div>
+    <!-- /#wrapper -->
+<?php include "includes/admin_footer.php" ?>
