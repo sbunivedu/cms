443efa4803294b10ea179649256b428bcc0caf51
Baochuan Lu <lubaochuan@gmail.com>
2020-12-10 12:21:38 -0600

    selective include and create post in admin

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function confirm_query($result){
+  global $connection;
+
+  if(!$result){
+    die("QUERY FAILED" . mysqli_error($connection));
+  }
+}
+
 function insert_categories(){
   global $connection;
 
