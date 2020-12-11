555a7e58dcbb04272fa42261fc5e9d3d1f4bf79f
Baochuan Lu <lubaochuan@gmail.com>
2020-12-11 15:12:25 -0600

    add category page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -23,12 +23,13 @@
       <div class="col-lg-12">
         <ul class="list-unstyled">
 <?php
-$query = "SELECT * FROM categories LIMIT 3";
+$query = "SELECT * FROM categories";
 $select_all_categories_sidebar = mysqli_query($connection, $query);
 while($row = mysqli_fetch_assoc($select_all_categories_sidebar)){
   $cat_title = $row['cat_title'];
+  $cat_id = $row['cat_id'];
 ?>
-          <li><a href="#"><?=$cat_title?></a>
+          <li><a href="category.php?category=<?= $cat_id ?>"><?=$cat_title?></a>
           </li>
 <?php
 }
