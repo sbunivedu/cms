6839dc71f820e6bc9effa5e1ed87d2e7776327f6
Baochuan Lu <lubaochuan@gmail.com>
2020-12-29 15:52:20 -0600

    fix db_script, add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Setup
+
+* create database "cms"
+* run SQL code in db_script.sql
+* default MySQL root account is used: root ""
--- db_script.sql
+++ db_script.sql
@@ -2,9 +2,9 @@ CREATE TABLE `categories` (
  `cat_id` int(11) NOT NULL AUTO_INCREMENT,
  `cat_title` varchar(255) NOT NULL,
  PRIMARY KEY (`cat_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `post` (
+CREATE TABLE `posts` (
  `post_id` int(3) NOT NULL AUTO_INCREMENT,
  `post_category_id` int(11) NOT NULL,
  `post_title` varchar(255) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE `post` (
  `post_comment_count` int(11) NOT NULL,
  `post_status` varchar(255) NOT NULL DEFAULT 'draft',
  PRIMARY KEY (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `post` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES (NULL, '1', 'Hello world!', 'blu', '2020-11-11', '', 'Wow!', 'blu, hello, php', '0', 'draft');
 
