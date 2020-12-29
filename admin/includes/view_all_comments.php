2d35d966099287b99d45b99352a77c297bd7115d
Baochuan Lu <lubaochuan@gmail.com>
2020-12-29 16:06:47 -0600

    relate comments to posts

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -33,7 +33,15 @@ while($row = mysqli_fetch_assoc($select_comments)){
   <td><?= $comment_content ?></td>
   <td><?= $comment_email ?></td>
   <td><?= $comment_status ?></td>
-  <td>Some Title</td>
+<?php
+$query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+$select_post_id_query = mysqli_query($connection, $query);
+while($row = mysqli_fetch_assoc($select_post_id_query)){
+  $post_id = $row['post_id'];
+  $post_title = $row['post_title'];
+}
+ ?>
+  <td><a href="../post.php?p_id=<?= $post_id ?>"><?= $post_title ?></a></td>
   <td><?= $comment_date ?></td>
   <td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>
   <td><a href='posts.php?delete='>Unapprove</a></td>
