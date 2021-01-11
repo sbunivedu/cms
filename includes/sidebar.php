add1749b084b0e5b531f42a050913daaa326b221
Baochuan Lu <lubaochuan@gmail.com>
2021-01-11 10:17:22 -0600

    login user with username password

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,24 @@
     <!-- /.input-group -->
   </div>
 
+  <!-- Login -->
+  <div class="well">
+    <h4>Login</h4>
+    <form action="includes/login.php" method="post">
+      <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Enter Username"/>
+      </div>
+      <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Enter password"/>
+        <span class="input-group-btn">
+          <button name="login" class="btn btn-primary" type="submit">
+            Submit
+        </button>
+        </span>
+      </div>
+    </form><!--search form-->
+    <!-- /.input-group -->
+  </div>
   <!-- Blog Categories Well -->
   <div class="well">
     <h4>Blog Categories</h4>
