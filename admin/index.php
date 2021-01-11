ecd91766d4adaf9227aadd790ddc9e1aa38c7e48
Baochuan Lu <lubaochuan@gmail.com>
2021-01-11 12:44:25 -0600

    add dynamic data to widgets

--- admin/index.php
+++ admin/index.php
@@ -26,7 +26,12 @@
                   <i class="fa fa-file-text fa-5x"></i>
                 </div>
                 <div class="col-xs-9 text-right">
-                <div class='huge'>12</div>
+<?php
+$query = "SELECT * FROM posts";
+$select_all_post = mysqli_query($connection, $query);
+$post_count = mysqli_num_rows($select_all_post);
+?>
+                <div class='huge'><?=$post_count?></div>
                 <div>Posts</div>
               </div>
             </div>
@@ -48,7 +53,12 @@
                   <i class="fa fa-comments fa-5x"></i>
                 </div>
                 <div class="col-xs-9 text-right">
-                  <div class='huge'>23</div>
+<?php
+$query = "SELECT * FROM comments";
+$select_all_comments = mysqli_query($connection, $query);
+$comment_count = mysqli_num_rows($select_all_comments);
+?>
+                  <div class='huge'><?=$comment_count?></div>
                   <div>Comments</div>
                 </div>
               </div>
@@ -70,7 +80,12 @@
                   <i class="fa fa-user fa-5x"></i>
                 </div>
                 <div class="col-xs-9 text-right">
-                  <div class='huge'>23</div>
+<?php
+$query = "SELECT * FROM users";
+$select_all_users = mysqli_query($connection, $query);
+$user_count = mysqli_num_rows($select_all_users);
+?>
+                  <div class='huge'><?=$user_count?></div>
                   <div> Users</div>
                 </div>
               </div>
@@ -92,7 +107,12 @@
                   <i class="fa fa-list fa-5x"></i>
                 </div>
                 <div class="col-xs-9 text-right">
-                  <div class='huge'>13</div>
+<?php
+$query = "SELECT * FROM categories";
+$select_all_categories = mysqli_query($connection, $query);
+$category_count = mysqli_num_rows($select_all_categories);
+?>
+                  <div class='huge'><?=$category_count?></div>
                   <div>Categories</div>
                 </div>
               </div>
