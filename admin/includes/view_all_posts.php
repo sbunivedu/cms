f26b166be7b22f6fc7cc7aa724986dcf32af3f1d
Baochuan Lu <lubaochuan@gmail.com>
2021-01-14 14:15:36 -0600

    use bootstrap modal for delete post confirmation

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,6 @@
 <?php
+include "delete_modal.php";
+
 if(isset($_POST['checkBoxArray'])){
   foreach($_POST['checkBoxArray'] as $post_id){
     $bulk_options = $_POST['bulk_options'];
@@ -92,7 +94,7 @@ while($row = mysqli_fetch_assoc($select_categories_id)){
   <td><?= $post_date ?></td>
   <td><a href='../post.php?p_id=<?= $post_id ?>'>View Post</a></td>
   <td><a href='posts.php?source=edit_post&p_id=<?= $post_id ?>'>Edit</a></td>
-  <td><a onClick='return confirm("Are you sure you want to delete?");' href='posts.php?delete=<?= $post_id ?>'>Delete</a></td>
+  <td><a href='javacript:void(0)' rel="<?= $post_id ?>" class="delete_link">Delete</a></td>
   </tr>
 <?php
 }
@@ -110,3 +112,14 @@ if(isset($_GET['delete'])){
   header("Location: posts.php");
 }
 ?>
+
+<script>
+$(document).ready(function(){
+  $(".delete_link").on("click", function(){
+    var id = $(this).attr("rel");
+    var delete_url = "posts.php?delete="+id;
+    $(".modal_delete_link").attr("href", delete_url);
+    $("#myModal").modal("show");
+  });
+});
+</script>
